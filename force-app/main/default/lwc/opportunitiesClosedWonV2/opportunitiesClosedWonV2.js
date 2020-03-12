39ae4914c1957e8f8ea4eb47260ee62035f53766
Maxwell Handler <mhandler@corp.fivepointcapital.com>
2020-03-12 13:18:43 -0700

    + Revised Opp Closed Won

---
+++ force-app/main/default/lwc/opportunitiesClosedWonV2/opportunitiesClosedWonV2.js
@@ -0,0 +1,82 @@
+import { LightningElement, wire, api, track } from 'lwc';
+// Import APEX Class Requirements
+import getOppList from '@salesforce/apex/RelatedOpportunitySearch.getOpportunityInfo';
+// Import Record Information
+import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
+// Import Navigatoins
+import { NavigationMixin } from 'lightning/navigation';
+// Import Fields from the Salesforce Schema
+import OPP_NAME from '@salesforce/schema/Opportunity.Name';
+import ACC_ID from '@salesforce/schema/Opportunity.AccountId';
+
+// Define Variables
+var name;
+var accountId;
+
+export default class OpportunitiesClosedWonV2 extends NavigationMixin(LightningElement) {
+    
+    // Grab Record Id
+    @api recordId;
+    // Grab Objects API Name
+    @api objectApiName;
+    // Track Fields
+    @track nameField;
+    @track accId;
+
+    name;
+    accountId;
+
+    @wire(getRecord, {recordId: '$recordId', fields: [OPP_NAME, ACC_ID] })
+    account({error, data}) {
+        if(data) {
+            this.nameField = getFieldValue(data, OPP_NAME);
+            this.accId = getFieldValue(data, ACC_ID);
+
+            // Log Values for Opportunity Name
+            console.log("Opportunity Name Values:");
+            console.log(this.nameField);
+            name = this.nameField;
+            console.log(name);
+
+            // Log Values for Account Id of Opportunity
+            console.log("Account Id Values:")
+            console.log(this.accId);
+            accountId = this.accId;
+            console.log(accountId);
+
+        } else if (error) {
+
+            // Log Error
+            console.log(error);
+        }
+    }
+
+    // Grab related Opportunities
+    @wire(getOppList, {recordId: '$accId'}) 
+    opportunities;
+
+    // *********************
+    // ACCORDION HANDLER
+    // *********************
+
+    // Handle Accordion
+    handleSectionToggle(event) {
+        const openSections = event.detail.openSections;
+    }
+
+    // *********************
+    // Open SubTab
+    // *********************
+    navigateToRecordViewPage(event) {
+        console.log('Clicked!');
+        console.log(event.target.dataset.id);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.target.dataset.id,
+                objectApiName: 'Opportunity',
+                actionName: 'view'
+            },
+        });
+    }
+}
